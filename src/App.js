71cd8d840c50dcd94dcef2d7ea593ab3d1a325fa
Your Name <btrangcal@gmail.com>
2019-02-19 15:47:57 -0800

    heroku initial commit

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+//mport { BrowserRouter as Router, Route, Link } from "react-router-dom";  
 import './App.css';
 import Home from './Home';
 import Layout from './hoc/Layout';
